c7257823766bc0a204cbf1c8462a2e4e9a2d0e7d
Rafał Rączka <info@rafalraczka.com>
2022-03-19 00:27:04 +0100

    helpful: create init-helpful module

--- lisp/init-helpful.el
+++ lisp/init-helpful.el
@@ -1,4 +1,4 @@
-;;; init.el --- Initialization file for Emacs -*- lexical-binding: t -*-
+;;; init-helpful.el --- Configuration of the helpful package -*- lexical-binding: t -*-
 
 ;; Copyright (C) 2022 Rafał Rączka <info@rafalraczka.com>
 
@@ -24,28 +24,8 @@
 
 ;;; Code:
 
-(add-to-list 'load-path (expand-file-name "lisp" user-emacs-directory))
+(straight-use-package 'helpful)
 
-(require 'my-init)
-(require 'init-straight)
-(require 'init-package)
+(provide 'init-helpful)
 
-(require 'my-envi)
-
-(require 'init-calendar)
-(require 'init-custom)
-(require 'init-ess)
-(require 'init-helpful)
-(require 'init-magit)
-
-(when (not my-envi-android)
-  (require 'init-olivetti))
-
-(require 'init-selectrum)
-(require 'init-tab-bar)
-(require 'init-undo-tree)
-(require 'init-which-key)
-
-(provide 'init)
-
-;;; init.el ends here
+;;; init-helpful.el ends here
