02017b2043eab119e3c80cfffe56302a05527c67
Rafał Rączka <info@rafalraczka.com>
2022-04-02 18:37:44 +0200

    dired: hide details by default on android

--- lisp/init-dired.el
+++ lisp/init-dired.el
@@ -39,6 +39,9 @@
     (progn (setq dired-listing-switches "-Ahl")
            (setq ls-lisp-dirs-first t)))
 
+  (when my-envi-android
+    (add-hook 'dired-mode-hook 'dired-hide-details-mode))
+
   )
 
 (provide 'init-dired)
