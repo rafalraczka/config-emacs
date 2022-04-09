7b1ba1801622d4ca55f6be64fa0d9f145f7c4660
Rafał Rączka <info@rafalraczka.com>
2022-04-09 19:09:40 +0200

    my-mini-gui: define mode for hiding GUI elements

--- lisp/my-mini-gui.el
+++ lisp/my-mini-gui.el
@@ -24,6 +24,43 @@
 
 ;;; Code:
 
+(defgroup my-mini-gui nil
+  "Minimal Emacs GUI."
+  :group 'frames)
+
+(defcustom my-mini-gui-disabled-modes '(menu-bar-mode
+                                        my-mini-gui-toggle-frame-decoration
+                                        scroll-bar-mode
+                                        tool-bar-mode
+                                        tooltip-mode)
+  "Modes which will be disabled with `my-mini-gui-mode'.
+This variable can take list of any functions which accept
+argument for default mode toggling, and only this argument, as a
+first one, will be used.  -1 will be passed to these functions
+while the `my-mini-gui-mode' is enabled, this will disable chosen
+modes, and +1 will be passed when `my-mini-gui-mode' is disabled
+and specified modes will be enabled."
+  :type 'hook
+  :group 'my-mini-gui)
+
+(defvar my-mini-gui-global-mode nil)
+
+(defun my-mini-gui-toggle-frame-decoration (&optional arg)
+  "Toggle frame decorating elements of the Emacs GUI.
+Enable decoration if ARG is nil, omitted, or is a positive
+number.  Disable if ARG is a negative number."
+  (if (and arg (<= arg 0))
+      (push '(undecorated . t) default-frame-alist)
+    (setq default-frame-alist (delete '(undecorated . t) default-frame-alist))))
+
+(define-minor-mode my-mini-gui-mode
+  "Mode which disable some of the Emacs GUI elements."
+  :global t
+  (if my-mini-gui-global-mode
+      (mapc (lambda (f) (funcall f 1)) my-mini-gui-disabled-modes)
+    (mapc (lambda (f) (funcall f -1)) my-mini-gui-disabled-modes))
+  (setq my-mini-gui-global-mode (not my-mini-gui-global-mode)))
+
 (provide 'my-mini-gui)
 
 ;;; my-mini-gui.el ends here
