6ac5ce26f1d3952a2c3ae4e10b91df16dc9493bc
Rafał Rączka <info@rafalraczka.com>
2022-04-09 17:23:30 +0200

    projectile: enable after first interaction

--- lisp/init-projectile.el
+++ lisp/init-projectile.el
@@ -26,7 +26,7 @@
 
 (my/package-ensure 'projectile)
 
-(add-hook 'after-init-hook 'projectile-mode)
+(add-hook 'my-init-first-interaction-hook #'projectile-mode)
 
 (with-eval-after-load 'projectile
 
