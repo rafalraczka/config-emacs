5b8a5344324f512fe23157bfebc8983f318ecb67
Rafał Rączka <info@rafalraczka.com>
2022-08-02 20:50:45 +0200

    lsp: disable lsp-command-map
    Signed-off-by: Rafał Rączka <info@rafalraczka.com>

--- lisp/config/config-lsp-mode.el
+++ lisp/config/config-lsp-mode.el
@@ -28,6 +28,7 @@
 (require 'which-key)
 
 (setq lsp-headerline-breadcrumb-enable nil)
+(setq lsp-keymap-prefix nil)
 (setq lsp-signature-auto-activate nil)
 
 (add-hook 'lsp-mode-hook #'company-mode)
