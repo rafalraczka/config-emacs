6f380a161ddc51cca3663ee60b41394eab0c5493
Rafał Rączka <info@rafalraczka.com>
2022-08-03 23:07:04 +0200

    vterm: install
    Signed-off-by: Rafał Rączka <info@rafalraczka.com>

--- lisp/config/config.el
+++ lisp/config/config.el
@@ -361,6 +361,9 @@
   :install 'package
   :execute ((add-hook 'core-utils-first-interaction-hook #'volatile-highlights-mode)))
 
+(core-use-module 'vterm
+  :install 'package)
+
 (core-use-module 'which-key
   :after 'which-key
   :install 'package
